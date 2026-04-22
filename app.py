1a90295a975405a16c849610d901807648b202a7
danialissham02 <danialissham2@gmail.com>
2026-04-22 10:10:04 +0800

    debug mail config

--- app.py
+++ app.py
@@ -27,12 +27,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Mail config — uses Mailtrap
 app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'invoiceflow@demo.com'
-
+app.config['MAIL_DEBUG'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
